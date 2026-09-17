d90d602b4d48e7e8e007c6117f78b7afcd6453fa
myoan <motoki.yoan@gmail.com>
2026-09-18 07:44:53 +0900

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,20 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.mapleader = " "
+
+vim.opt.backup = false
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.list = true
+vim.opt.ruler = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.termguicolors = true
+vim.opt.winblend = 5
+vim.opt.pumblend = 5
+vim.opt.clipboard = "unnamedplus"
+vim.opt.autoread = true
+vim.api.nvim_command("set listchars=tab:>-,trail:~,nbsp:%,extends:>,precedes:<,eol:$")
+
 require("config.lazy")
--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,13 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+vim.keymap.del({ "n", "x", "o" }, "s")
 
--- LazyVim binds <C-s> to ":w<cr>" in i/x/n/s modes; free it back up.
-vim.keymap.del({ "i", "x", "n", "s" }, "<C-s>")
+local options = { noremap = true, silent = true }
+vim.keymap.set("i", "<C-a>", "^", options)
+vim.keymap.set("i", "<C-e>", "$", options)
+vim.keymap.set("n", "<C-a>", "^", options)
+vim.keymap.set("n", "<C-e>", "$", options)
+vim.keymap.set("n", "<C-j>", "<C-w>", options)
+vim.keymap.set("n", "<Esc><Esc>", "<cmd>:nohlsearch<Enter>", options)
 
--- Shell-style line navigation.
--- This overrides normal-mode <C-a>/<C-e> (increment number / scroll down line).
-vim.keymap.set("i", "<C-a>", "<C-o>^", { desc = "Go to start of line" })
-vim.keymap.set("i", "<C-e>", "<C-o>$", { desc = "Go to end of line" })
-vim.keymap.set("n", "<C-a>", "^", { desc = "Go to start of line" })
-vim.keymap.set("n", "<C-e>", "$", { desc = "Go to end of line" })
+-- vim.keymap.set("n", "<leader>ff", "<cmd>:Telescope find_files hidden=true<Enter>", options)
